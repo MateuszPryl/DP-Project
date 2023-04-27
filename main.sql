1b8a7ffdc49ce6b0680732313c61c1cb1529c258
Mateusz <mateusz.pryl@gmail.com>
2023-04-27 16:29:45 +0200

    Initial commit

---
+++ main.sql
@@ -0,0 +1,35 @@
+/*
+    # Section 1    
+    DB and tables creation
+*/
+
+
+
+
+
+/*
+    # Section 2  
+    Functions declarations
+*/
+
+
+
+
+
+/*
+    # Section 3
+    Procedures declarations
+*/
+
+
+
+
+
+/*
+    # Section 4
+    Triggers declarations
+*/
+
+
+
+
